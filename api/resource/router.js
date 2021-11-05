7565e81d405dcedacc4cf2ab06834ac922178a10
jillig3 <jacobillig@gmail.com>
2021-11-05 13:15:04 -0500

    res router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,30 @@
-// build your `/api/resources` router here
+const express = require('express')
+const router = express.Router()
+
+const Resources = require('./model')
+
+router.get('/', (req, res, next) => {
+    Resources.get()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+    Resources.getById(req.params.id)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Resources.create(req.body)
+        .then(newResource => {
+            res.status(201).json(newResource)
+        })
+        .catch(next)
+})
+
+module.exports = router;

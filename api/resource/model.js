5e79d42e74a8610cd16ef9bb9075d47c305b227e
jillig3 <jacobillig@gmail.com>
2021-11-05 13:10:28 -0500

    res model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,23 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function get() {
+    return db('resources')
+}
+
+function getById(id) {
+    return db('resources')
+        .where('resource_id', id)
+        .first()
+}
+
+async function create(newResource) {
+    const result = await db('resources')
+        .insert(newResource)
+    return getById(result)
+}
+
+module.exports = {
+    get,
+    getById,
+    create
+}

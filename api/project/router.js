ae0e197d377a791e7c6adba8ac3c8ea674fdbeb9
jillig3 <jacobillig@gmail.com>
2021-11-05 13:07:21 -0500

    mode router

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,34 @@
-// build your `/api/projects` router here
+const express = require('express')
+const router = express.Router()
+
+const Projects = require('./model')
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            next(err)
+        })
+})
+
+router.get('/:id', (res, req, next) => {
+    Projects.getById(req.params.id)
+        .then(task => {
+            res.status(200).json(task)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Projects.create(req.body)
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(err => {
+            next(err)
+        })
+})
+
+module.exports = router;
